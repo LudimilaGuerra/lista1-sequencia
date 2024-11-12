337aaab27399d5292a7f11e0b85db8dc851307f1
Ludimila Guerra <167739772+LudimilaGuerra@users.noreply.github.com>
2024-11-12 15:56:27 -0300

    Update ex2.c

--- ex2.c
+++ ex2.c
@@ -11,6 +11,8 @@ int main()
 	printf("Agora, aplique localização do ponto B (x y): \n");
 	scanf("%f %f", &x2, &y2);
 
+	//sqrt é o comando para aplicar a raiz quadrada no número que estiver dentro do parenteses
+	/*pow é o comando para realizar potenciação, colocando o numero elevado dentro do parenteses e apos a virgula*/
 	d = sqrt(pow((x1-x2),2)+pow((y1-y2),2));
 
 	printf("A distancia dos pontos A e B corresponde a: %.2f", d);
